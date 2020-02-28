0c85882a4ee580276db0f4e3ec7836321ca4c037
Sahand Amini <sahandaamini@gmail.com>
2020-02-27 21:35:03 -0700

    Implemented changes to original SimpleList and added version 2 functions

--- CSE360_Assignment2/src/cse360assign2/SimpleList.java
+++ CSE360_Assignment2/src/cse360assign2/SimpleList.java
@@ -1,5 +1,8 @@
 package cse360assign2;
 
+import java.util.*;
+import java.lang.Math;
+
 public class SimpleList { // class declaring list and count variables
 	int[] list;
 	int count;
@@ -11,13 +14,22 @@ public SimpleList() { // constructor method initializing list and count variable
 }
 
 public void add(int parameter) {
+	int arraySize = list.length;
+	if (count < arraySize) { // ensures count does not exceed array size
+		count++;
+	}
+	else {
+		int newSize = (int) Math.floor(list.length * 1.5); // if no room in array, makes new array 50% larger
+		arraySize = newSize;
+		int[] newList = Arrays.copyOf(list, arraySize); // copies elements into new array
+		list = newList; // uses new array with padding
+		count++;
+	}
+	
 	for(int index = count - 1; index >= 0; index--) { // shifts values in array right by 1
 		if (index != list.length-1) { // ensures last value does not go out of bounds 
 			list[index + 1] = list[index];
 		}
-	}
-	if (count < 10) { // ensures count does not exceed array size
-		count++;
 	}
 	
 	list[0] = parameter; // adds parameter at index 0
@@ -33,6 +45,13 @@ public void remove(int parameter) {
 			list[index] = list[index+1];
 			index++;
 		}
+	}
+	
+	list[count] = 0; // sets last element to zero
+	
+	if (count <= ((int) Math.floor(list.length * 0.75))) { // resizes array if more than 25% empty elements
+		int[] newList = Arrays.copyOf(list, count); // copies elements into truncated array
+		list = newList;
 	}
 }
 
@@ -60,6 +79,27 @@ public int search(int number) {
 	}
 	
 	return location; // returns location of value in array
+}
+
+public void append(int parameter) {
+	if (count < list.length) { // adds parameter to array if array has room
+		list[count] = parameter;
+	}
+	else {
+		int newSize = (int) Math.floor(list.length * 1.5); // if no room in array, makes new array 50% larger
+		int[] newList = Arrays.copyOf(list, newSize); // copies elements into new array
+		list = newList; // uses new array with padding
+		list[count] = parameter; // adds parameter to new array
+	}
+	count++; // increments count
+}
+
+public int first() { // returns first element in array
+	return list[0];
+}
+
+public int size() { // returns number of empty elements in array
+	return list.length-count;
 }
 
 }
